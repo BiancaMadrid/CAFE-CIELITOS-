ece21feec76efc041daf78ed0254c11874e8dd62
Bianca Madrid <bugamadrid111@gmail.com>
2024-11-13 20:07:56 -0700

    Meat Enum
    Contains meat options, base cost for meat and additional cost if more meat is added onto sandwich.

--- CafeCieltosCapstoneProject2/src/main/java/com/pluralsight/MeatChoice.java
+++ CafeCieltosCapstoneProject2/src/main/java/com/pluralsight/MeatChoice.java
@@ -2,10 +2,33 @@ package com.pluralsight;
 
 public enum MeatChoice {
         Steak, HAM, SALAMI, ROAST_BEEF, CHICKEN, BACON;
+    Steak, HAM, SALAMI, ROAST_BEEF, CHICKEN, BACON;
 
 //        public double getCost(SandwichSize){
 //            double cost = 0.0;
 //            //Price based on Size
+    //Using Switch case to alternate meat options
+    public double getCost(sandwichSize size) {
+        switch (size) {
+            case FOUR_INCH:
+                return 1.00;
+            case EIGHT_INCH:
+                return 2.00;
+            case TWELVE_INCH:
+                return 3.00;
+            default:
+                return 0.00;
+        }
 
     }
+    //Using switch case to add extra cost to add additional meat
+    public double getExtraCost(sandwichSize size){
+        switch (size){
+            case FOUR_INCH: return 0.50;
+            case EIGHT_INCH: return 1.00;
+            case TWELVE_INCH: return 1.50;
+            default: return 0.00;
+        }
+    }
+}
 
