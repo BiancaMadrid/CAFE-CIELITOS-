c4932c65c2fced99c2c9656e0337f7d1e9f85758
Bianca Madrid <bugamadrid111@gmail.com>
2024-11-14 20:40:28 -0700

    Created menu
    Created the menu and added reader as my scanner to allow user input

--- CafeCieltosCapstoneProject2/src/main/java/com/pluralsight/Main.java
+++ CafeCieltosCapstoneProject2/src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,21 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    private static Scanner reader = new Scanner(System.in);
 
+    public static void main(String[] args) {
+    //Making HomeScreen w/ order option and exit option
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Welcome to Café Cielitos");
+            System.out.println("1. New Order");
+            System.out.println("2. Display Menu");
+            System.out.println("3. Exit");
+            System.out.print("Please select an option: ");
+
+            int choice = reader.nextInt();
+            reader.nextLine(); // Consume newline
 
         System.out.println("Test One");
     }
